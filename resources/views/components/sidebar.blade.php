04e6f352679c4f3bcf9daac7d54a025665d865fe
Archer <code.archer70@gmail.com>
2018-05-07 15:19:55 -0700

    Add sidebar login form.

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,3 +1,32 @@
+@if (!Auth::check())
+    <div class="card mb-4">
+        <div class="card-body">
+            <h5 class="card-title">@lang('Login')</h5>
+            <form method="post" action="{{ route('login') }}">
+                @csrf
+                <div class="form-group">
+                    <input class="form-control" type="email" name="email" placeholder="Email Address">
+                </div>
+                <div class="form-group">
+                    <input class="form-control" type="password" name="password" placeholder="Password">
+                </div>
+                <div clas="form-group">
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> @lang('Remember Me')
+                        </label>
+                    </div>
+                </div>
+                <div class="form-group mb-0">
+                    <button type="submit" class="btn btn-primary">
+                        @lang('Login')
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endif
+
 @if (View::hasSection('page_actions'))
     <div class="card mb-4">
         <div class="card-body">
